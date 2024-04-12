1a0d49faa93aaff02e61718d5632bf5f60a24390
jamibear <5701ja.mi@gmail.com>
2024-04-12 19:36:14 +0800

    add building section

---
+++ src/components/spinner.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const Spinner = () => {
+    const [index, setIndex] = useState<number>(0);
+    const dots: string[] = ['⠾', '⠽', '⠻', '⠯', '⠷',];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % dots.length);
+        }, 150); // Change the interval duration as needed
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div>
+            {dots[index]} BUILDING
+        </div>
+    );
+};
+
+export default Spinner;
+
